105a0f433af04e29fcf6830d3fd1bb599e74911e
breakitfixit <163203848+breakitfixit@users.noreply.github.com>
2024-04-01 22:52:56 +0200

    opdracht voltooid

--- variables.js
+++ variables.js
@@ -8,37 +8,53 @@ const start = "Let's go!";
 // 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 
+const name = "Rick"
 
 // ==========================================
 // 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 
+const eyes = "Green"
 
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let age = "37"
 
 // ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let profession = "Data Co-ordinator"
 
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 
+const shoesize = "9.5"
 
 // ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let partner = "yes"
 
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let howManyKids = "1"
 
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
 // ==========================================
+
+console.log(start)
+console.log(name)
+console.log(eyes)
+console.log(age)
+console.log(profession)
+console.log(shoesize)
+console.log(partner)
+console.log(howManyKids)
